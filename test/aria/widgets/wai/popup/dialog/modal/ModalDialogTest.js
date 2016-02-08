e42525c978e10d03354ef50a79ab0a3b6dae5ced
ymeine <yannick.meine@gmail.com>
2016-02-08 17:47:55 +0100

    EnhancedRobotTestCase - simplified test.aria.widgets.wai.popup.dialog.modal.ModalDialogTest

--- test/aria/widgets/wai/popup/dialog/modal/ModalDialogTest.js
+++ test/aria/widgets/wai/popup/dialog/modal/ModalDialogTest.js
@@ -17,7 +17,6 @@ var Aria = require('ariatemplates/Aria');
 
 var ariaUtilsJson = require('ariatemplates/utils/Json');
 var ariaUtilsString = require('ariatemplates/utils/String');
-var ariaUtilsType = require('ariatemplates/utils/Type');
 
 var ariaUtilsDom = require('ariatemplates/utils/Dom');
 
@@ -115,12 +114,12 @@ Dialog.prototype.open = function () {
 
 module.exports = Aria.classDefinition({
     $classpath : "test.aria.widgets.wai.popup.dialog.modal.ModalDialogTest",
-    $extends : require('ariatemplates/jsunit/RobotTestCase'),
+    $extends : require('test/EnhancedRobotTestCase'),
 
     $constructor : function () {
         // ------------------------------------------------------ initialization
 
-        this.$RobotTestCase.constructor.call(this);
+        this.$EnhancedRobotTestCase.constructor.call(this);
 
         // ---------------------------------------------------------- processing
 
@@ -138,295 +137,6 @@ module.exports = Aria.classDefinition({
     },
 
     $prototype : {
-        ////////////////////////////////////////////////////////////////////////
-        // Library: helpers
-        ////////////////////////////////////////////////////////////////////////
-
-        getActiveElement : function () {
-            return Aria.$global.window.document.activeElement;
-        },
-
-        getWidgetDom : function (id) {
-            var widgetInstance = this.getWidgetInstance(id);
-            return widgetInstance.getDom();
-        },
-
-
-
-        ////////////////////////////////////////////////////////////////////////
-        // Library: asynchronous processing
-        ////////////////////////////////////////////////////////////////////////
-
-        _asyncIterate : function (array, callback, onend, thisArg) {
-            // -------------------------------------- input arguments processing
-
-            if (thisArg === undefined) {
-                thisArg = this;
-            }
-
-            // ------------------------------------------------------ processing
-
-            var index = 0;
-
-            function iterate () {
-                var currentIndex = index;
-
-                if (currentIndex >= array.length) {
-                    onend.call(thisArg, array);
-                } else {
-                    index++;
-
-                    var item = array[currentIndex];
-                    callback.call(thisArg, iterate, item, currentIndex, array);
-                }
-            }
-
-            iterate();
-        },
-
-        _asyncSequence : function (functions, callback, thisArg) {
-            this._asyncIterate(functions, function (next, fn) {
-                fn.call(this, next);
-            }, callback, thisArg);
-        },
-
-        _localAsyncSequence : function (create, callback) {
-            // ------------------------------------------------------ processing
-
-            var self = this;
-
-            var sequence = [];
-
-            function add(fn) {
-                if (ariaUtilsType.isString(fn)) {
-                    fn = self[fn];
-                }
-
-                if (arguments.length > 1) {
-                    var boundArguments = Array.prototype.slice.call(arguments, 1);
-                    fn = self._partializeAsynchronousFunction(fn, boundArguments);
-                }
-
-                sequence.push(fn);
-            }
-
-            create.call(this, add);
-
-            // ------------------------------------------------------ delegation
-
-            return this._asyncSequence(sequence, callback, this);
-        },
-
-        _partializeAsynchronousFunction : function (fn, boundArguments) {
-            return function (next) {
-                var additionalArguments = Array.prototype.slice.call(arguments, 1);
-
-                var allArguments = [];
-                allArguments.push(next);
-                allArguments.push.apply(allArguments, boundArguments);
-                allArguments.push.apply(allArguments, additionalArguments);
-
-                return fn.apply(this, allArguments);
-            };
-        },
-
-
-
-        ////////////////////////////////////////////////////////////////////////
-        // Library: Wait & check
-        ////////////////////////////////////////////////////////////////////////
-
-        _waitForFocus : function (callback, element, strict, thisArg) {
-            // -------------------------------------- input arguments processing
-
-            if (strict == null) {
-                strict = true;
-            }
-
-            if (thisArg === undefined) {
-                thisArg = this;
-            }
-
-            // ------------------------------------------------------ processing
-
-            this.waitFor({
-                scope: thisArg,
-                condition: condition,
-                callback: callback
-            });
-
-            // -----------------------------------------------------------------
-
-            function condition() {
-                var activeElement = this.getActiveElement();
-
-                var result;
-                if (strict) {
-                    result = activeElement === element;
-                } else {
-                    result = ariaUtilsDom.isAncestor(activeElement, element);
-                }
-
-                return result;
-            }
-        },
-
-        _waitAndCheck : function (callback, condition, message, thisArg) {
-            // -------------------------------------- input arguments processing
-
-            if (thisArg === undefined) {
-                thisArg = this;
-            }
-
-            // ------------------------------------------------------ processing
-
-            this.waitFor({
-                scope: thisArg,
-                condition: condition,
-                callback: check
-            });
-
-            function check() {
-                this.assertTrue(condition.call(thisArg), message);
-                callback();
-            }
-        },
-
-        ////////////////////////////////////////////////////////////////////////
-        // Library: User actions
-        ////////////////////////////////////////////////////////////////////////
-
-        _type : function (callback, sequence) {
-            this.synEvent.type(null, sequence, callback);
-        },
-
-
-
-        _pushKey : function (callback, key) {
-            this._type(callback, '[<' + key + '>]');
-        },
-
-        _releaseKey : function (callback, key) {
-            this._type(callback, '[>' + key + '<]');
-        },
-
-        _pressKey : function (callback, key) {
-            this._type(callback, '[' + key + ']');
-        },
-
-
-
-        _pushShift : function (callback) {
-            this._pushKey(callback, 'shift');
-        },
-
-        _releaseShift : function (callback) {
-            this._releaseKey(callback, 'shift');
-        },
-
-        _pressWithShift : function (callback, press) {
-            this._localAsyncSequence(function (add) {
-                add('_pushShift');
-                add(press);
-                add('_releaseShift');
-            }, callback);
-        },
-
-
-
-        _pressTab : function (callback) {
-            this._pressKey(callback, 'tab');
-        },
-
-        _pressF10 : function (callback) {
-            this._pressKey(callback, 'F10');
-        },
-
-        _pressShiftF10 : function (callback) {
-            this._pressWithShift(callback, this._pressF10);
-        },
-
-        _pressShiftTab : function (callback) {
-            this._pressWithShift(callback, this._pressTab);
-        },
-
-        _pressEnter : function (callback) {
-            this.synEvent.type(null, '[enter]', callback);
-        },
-
-        _pressEscape : function (callback) {
-            this.synEvent.type(null, '[escape]', callback);
-        },
-
-
-
-        _leftClick : function (callback) {
-            this.synEvent.click(null, callback);
-        },
-
-
-
-        ////////////////////////////////////////////////////////////////////////
-        // Library: manual navigation
-        ////////////////////////////////////////////////////////////////////////
-
-        _focusWidget : function (callback, id) {
-            // -----------------------------------------------------------------
-
-            this._asyncSequence([
-                focusElementBefore,
-                this._pressTab,
-                wait
-            ], callback, this);
-
-            // -----------------------------------------------------------------
-
-            function focusElementBefore(next) {
-                var elementBefore = this.getElementById('before_' + id);
-                elementBefore.focus();
-                this._waitForFocus(next, elementBefore);
-            }
-
-            function wait(next) {
-                var widgetDom = this.getWidgetDom(id);
-                this._waitForFocus(next, widgetDom, false);
-            }
-        },
-
-        _navigate : function (callback, action) {
-            // -----------------------------------------------------------------
-
-            var activeElement = this.getActiveElement();
-
-            this._asyncSequence([
-                action,
-
-                function (next) {
-                    this.waitFor({
-                        scope: this,
-                        condition: condition,
-                        callback: next
-                    });
-                }
-            ], callback, this);
-
-            // -----------------------------------------------------------------
-
-            function condition() {
-                return this.getActiveElement() !== activeElement;
-            }
-        },
-
-        _navigateForward : function (callback) {
-            this._navigate(callback, this._pressTab);
-        },
-
-        _navigateBackward : function (callback) {
-            this._navigate(callback, this._pressShiftTab);
-        },
-
-
-
         ////////////////////////////////////////////////////////////////////////
         // Library: Dialog management
         ////////////////////////////////////////////////////////////////////////
@@ -575,7 +285,7 @@ module.exports = Aria.classDefinition({
             // -----------------------------------------------------------------
 
             function getChildrenElements(next) {
-                widgetDom = this.getWidgetDom(id);
+                widgetDom = this._getWidgetDom(id);
 
                 var parentElement = widgetDom.parentElement;
                 children = parentElement.children;
@@ -695,8 +405,8 @@ module.exports = Aria.classDefinition({
             // -----------------------------------------------------------------
 
             function check(next) {
-                var activeElement = this.getActiveElement();
-                var widgetDom = this.getWidgetDom(dialog.id);
+                var activeElement = this._getActiveElement();
+                var widgetDom = this._getWidgetDom(dialog.id);
 
                 this.assertTrue(ariaUtilsDom.isAncestor(activeElement, widgetDom), 'Focus should be inside the dialog.');
 
@@ -727,12 +437,12 @@ module.exports = Aria.classDefinition({
             // -----------------------------------------------------------------
 
             function check(next) {
-                var activeElement = this.getActiveElement();
+                var activeElement = this._getActiveElement();
 
                 this._localAsyncSequence(function (add) {
                     add('_focusWidget', dialog.buttonId);
                     add(function (next) {
-                        var openingElement = this.getActiveElement();
+                        var openingElement = this._getActiveElement();
 
                         this.assertTrue(activeElement === openingElement, 'Focus was not restored properly on the button after closing the dialog.');
                         next();
